5c136592f6de44b29e75d8c8ba9576cb84d00182
gilang_gans <gilangsos99@gmail.com>
2022-05-16 12:52:39 +0700

    Tugas js 13
    TUgas ini untuk niomic

---
+++ javascript/tugas13.js
@@ -0,0 +1,10 @@
+function tugasUnshift(){
+    var buah = ['Pisang','Jeruk']
+    var buah2 = ['Pisang','Jeruk']
+    buah2.unshift('Apel','Mangga')
+    console.log(buah)
+    console.log(buah2)
+}
+
+
+tugasUnshift();
